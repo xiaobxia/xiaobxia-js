d3c87b577097b415a8a20072b3c2602ef5890e3b
xiaobxia <673806687@qq.com>
2016-11-17 22:21:11 +0800

    有对象的事件

--- 事件/事件注册.js
+++ 事件/事件注册.js
@@ -4,5 +4,9 @@ var addEvent = function(elem, eventName, handler) {
     } else if (elem.attachEvent) {
         //兼容ie
         elem.attachEvent("on" + eventName, handler);
+        //绑定上下文的，不过好像不能直接写在这个函数中，因为fn的参数是个问题
+        /*elem.attachEvent("on" + eventName,function (e) {
+            handler.call(elem,e);
+        })*/
     }
 };
