f0e9f4ec9ef8e4811f0c3e3de580310802839856
chenlingjie <chenlingjie@cd121.com>
2017-06-21 10:02:07 +0800

    添加url方法

---
+++ URL/mobile.js
@@ -0,0 +1,15 @@
+/**
+ * Created by xiaobxia on 2017/6/21.
+ */
+function load(url) {
+    var iFrame;
+    iFrame = document.createElement("iframe");
+    iFrame.setAttribute("src", url);
+    iFrame.setAttribute("style", "display:none;");
+    iFrame.setAttribute("height", "0px");
+    iFrame.setAttribute("width", "0px");
+    iFrame.setAttribute("frameborder", "0");
+    document.body.appendChild(iFrame);
+    iFrame.parentNode.removeChild(iFrame);
+    iFrame = null;
+}
