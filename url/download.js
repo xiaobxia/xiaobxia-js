89733c8542a5bf651407e7c64975fce2278af713
chenlingjie <chenlingjie@cd121.com>
2017-09-24 12:35:48 +0800

    修改

--- url/download.js
+++ url/download.js
@@ -1,10 +1,17 @@
 /**
  * Created by xiaobxia on 2017/9/24.
  */
-function downloadWordHandler(bingqingzsbh) {
+function downloadWordHandler() {
     let turnForm = document.createElement('form');
     turnForm.method = 'POST';
     turnForm.action = '';
+
+    let input1 = document.createElement('input');
+    input1.setAttribute('name', '');
+    input1.setAttribute('type', 'hidden');
+    input1.setAttribute('value', '');
+    turnForm.appendChild(input1);
+
     document.body.appendChild(turnForm);
     turnForm.submit();
     turnForm.parentNode.removeChild(turnForm);
