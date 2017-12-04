01ac421d6a6f767b05fa9efab626536b52677fc1
chenlingjie <chenlingjie@cd121.com>
2017-12-04 13:38:59 +0800

    xg

--- url/query.js
+++ url/query.js
@@ -1,5 +1,5 @@
 function addURLparam(url, name, value) {
-    url += (url.indexOf("?") == -1 ? "?" : "&");
+    url += (url.indexOf("?") === -1 ? "?" : "&");
     url += encodeURIComponent(name) + "=" + encodeURIComponent(value);
     return url;
 
@@ -10,10 +10,10 @@ function myaddURLparam(url, data) {
     if (typeof data === "undefined") {
         return false;
     }
-    if (data.indexOf("&") != -1) {
+    if (data.indexOf("&") !== -1) {
         parts = data.split("&");
         for (i = 0, l = parts.length; i < l; i++) {
-            url += (url.indexOf("?") == -1 ? "?" : "&");
+            url += (url.indexOf("?") === -1 ? "?" : "&");
             part = parts[i].split("=");
             name = encodeURIComponent(part[0]);
             value = encodeURIComponent(part[1]);
@@ -21,7 +21,7 @@ function myaddURLparam(url, data) {
         }
 
     } else {
-        url += (url.indexOf("?") == -1 ? "?" : "&");
+        url += (url.indexOf("?") === -1 ? "?" : "&");
         part = data.split("=");
         name = encodeURIComponent(part[0]);
         value = encodeURIComponent(part[1]);
@@ -36,7 +36,7 @@ function addqueryarg(url,args) {
     if (typeof args !== "object") {
         return false;
     }
-    url += (url.indexOf("?") == -1 ? "?" : "&");
+    url += (url.indexOf("?") === -1 ? "?" : "&");
     for(key in args){
         name=encodeURIComponent(key);
         value=encodeURIComponent(args[key]);
@@ -45,31 +45,4 @@ function addqueryarg(url,args) {
     url=url.substring(0,url.length-1);
     return url;
 }
-//网上的写法
-(function ($) {
-    $.extend({
-        Request: function (m) {
-            var sValue = location.search.match(new RegExp("[\?\&]" + m + "=([^\&]*)(\&?)", "i"));
-            return sValue ? sValue[1] : sValue;
-        },
-        UrlUpdateParams: function (url, name, value) {
-            var r = url;
-            if (r != null && r != 'undefined' && r != "") {
-                value = encodeURIComponent(value);
-                var reg = new RegExp("(^|)" + name + "=([^&]*)(|$)");
-                var tmp = name + "=" + value;
-                if (url.match(reg) != null) {
-                    r = url.replace(eval(reg), tmp);
-                }
-                else {
-                    if (url.match("[\?]")) {
-                        r = url + "&" + tmp;
-                    } else {
-                        r = url + "?" + tmp;
-                    }
-                }
-            }
-            return r;
-        }
-    });
-})(jQuery);
+
