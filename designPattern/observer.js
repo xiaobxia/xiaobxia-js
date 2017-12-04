3c4d68e974fa7dcce02fc7aa0237ef0e143f3ded
chenlingjie <chenlingjie@cd121.com>
2017-12-04 13:28:16 +0800

    xg

--- designPattern/observer.js
+++ designPattern/observer.js
@@ -1,3 +1,9 @@
+/**
+ * 观察者模式
+ *
+ * 思想在于自定义事件
+ * 注册事件然后分发事件
+ */
 var Observer = (function () {
     var message = {};
     return {
@@ -17,15 +23,15 @@ var Observer = (function () {
                     args: args || {}
                 },
                 len = message[type].length;
-            for (var i=0;i<len;i++){
-                message[type][i].call(this,events);
+            for (var i = 0; i < len; i++) {
+                message[type][i].call(this, events);
             }
         },
-        remove: function (type,fn) {
-            if(message[type] instanceof Array){
-                var i=message[type].length-1;
-                for(;i>=0;i--){
-                    message[type][i]===fn&&message[type].splice(i,1);
+        remove: function (type, fn) {
+            if (message[type] instanceof Array) {
+                var i = message[type].length - 1;
+                for (; i >= 0; i--) {
+                    message[type][i] === fn && message[type].splice(i, 1);
                 }
             }
         }
