7369f9242f5f481982415daf10ac7fb962bedae9
chenlingjie <chenlingjie@cd121.com>
2017-12-18 10:00:48 +0800

    xg

---
+++ environment/title.js
@@ -0,0 +1,40 @@
+/**
+ * Created by xiaobxia on 2017/12/18.
+ */
+//修改title
+//基于jQuery或Zepto
+function change_title(title){
+    document.title = title;
+    // hack在微信等webview中无法修改document.title的情况
+    var $iframe = $('<iframe src="/favicon.ico"></iframe>');
+    $iframe.on('load',function() {
+        setTimeout(function() {
+            $iframe.off('load').remove();
+        }, 0);
+    }).appendTo($('body'));
+}
+
+$('#demo1').on('click', function(){
+    change_title('demo1 title');
+});
+
+
+// 原生触发
+function changeTitle(title){
+    var body = document.getElementsByTagName('body')[0];
+    document.title = title;
+    var iframe = document.createElement("iframe");
+    iframe.setAttribute("src", "/favicon.ico");
+
+    iframe.addEventListener('load', function() {
+        setTimeout(function() {
+            iframe.removeEventListener('load');
+            document.body.removeChild(iframe);
+        }, 0);
+    });
+    document.body.appendChild(iframe);
+}
+
+document.getElementById('demo2').ontouchend = function(){
+    changeTitle('demo2 title');
+}
